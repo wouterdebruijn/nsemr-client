548231da7090ed9b2ec2c38c5e569ea4216bd967
Wouter de Bruijn <wouter@hedium.nl>
2021-09-09 22:13:52 +0200

    :art: Changed indenting to two spaces.

--- src/index.js
+++ src/index.js
@@ -1,43 +1,43 @@
 let serverIpElement
 
 window.addEventListener("load", () => {
-    serverIpElement = document.getElementById("server-ip");
-    setClickEvents();
+  serverIpElement = document.getElementById("server-ip");
+  setClickEvents();
 })
 
 const setClickEvents = (elements) => {
-    serverIpElement.addEventListener("click", () => {
-        document.execCommand("copy");
-    });
-
-    serverIpElement.addEventListener("copy", serverIpCopy)
+  serverIpElement.addEventListener("click", () => {
+    document.execCommand("copy");
+  });
+  
+  serverIpElement.addEventListener("copy", serverIpCopy)
 }
 
 const serverIpCopy = (event) => {
-    event.preventDefault();
-
-    if (event.clipboardData) {
-        event.clipboardData.setData("text/plain", serverIpElement.textContent)
-        createPop("Copied!", serverIpElement);
-    }
+  event.preventDefault();
+  
+  if (event.clipboardData) {
+    event.clipboardData.setData("text/plain", serverIpElement.textContent)
+    createPop("Copied!", serverIpElement);
+  }
 }
 
 const createPop = (text, parrent) => {
-    const existingChild = document.getElementById("pop-up");
-    if (existingChild)
-        parrent.removeChild(existingChild);
-
-    const child = document.createElement("span");
-    child.className = "pop-up"
-    child.id = "pop-up"
-    child.innerText = text;
-    parrent.appendChild(child);
-    timeoutKillingYounglings(parrent, "pop-up")
+  const existingChild = document.getElementById("pop-up");
+  if (existingChild)
+  parrent.removeChild(existingChild);
+  
+  const child = document.createElement("span");
+  child.className = "pop-up"
+  child.id = "pop-up"
+  child.innerText = text;
+  parrent.appendChild(child);
+  timeoutKillingYounglings(parrent, "pop-up")
 }
 
 const timeoutKillingYounglings = (parrent, childId) => {
-    setTimeout(() => {
-        const child = document.getElementById(childId);
-        parrent.removeChild(child);
-    }, 2500)
+  setTimeout(() => {
+    const child = document.getElementById(childId);
+    parrent.removeChild(child);
+  }, 2500)
 }
